437049f90e0af7539797dae946391c885861dce5
Juma Michael <obudho76@gmail.com>
2025-09-27 19:03:50 +0300

    allfiles

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 //make the button clickable and alert the user
-document.querySelector('button').addEventListener('click', function() {
-    alert('Button clicked!');
-});
+//document.querySelector('button').addEventListener('click', function() {
+  // alert('Button clicked!');
+//});
+
+// Variable declaration
+/**1. let
+ * 2. const
+ * 3. var --not recommended
+ * variables are containers for storing data values
+ */
+//  let x = 20;
+//  let y = 2;
+//  let z = x * y;
+//  console.log(z); 
+
+// let firstName = "Anthonio";
+// let lastName = "Ombati";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// const pi = 3.14;
+// console.log(pi);
+
+let x, y, z; 
+x = 5;
+y = 6;
+z = x + y;
+console.log(z);
+//calculate the age of a man born in 1990
+    let birthYear = 2004;
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    console.log(age);
